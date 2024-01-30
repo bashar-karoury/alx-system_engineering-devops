6897f789683056f0452849c271ab3fc9c5fb1bbc
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-30 23:38:04 +0400

    fix task 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].scan(/\[from:\(.*)\].*\[to:.*\].*\[flags:.*\]\s\[msg/).join
+puts ARGV[0].scan(/\[from:(.*)\].*\[to:.*\].*\[flags:.*\]\s\[msg/).join
 puts ","
 
-puts ARGV[0].scan(/\[from:\.*\].*\[to:(.*)\].*\[flags:.*\]\s\[msg/).join
+puts ARGV[0].scan(/\[from:.*\].*\[to:(.*)\].*\[flags:.*\]\s\[msg/).join
 puts ","
 
-puts ARGV[0].scan(/\[from:\.*\].*\[to:.*\].*\[flags:(.*)\]\s\[msg/).join
+puts ARGV[0].scan(/\[from:.*\].*\[to:.*\].*\[flags:(.*)\]\s\[msg/).join
 
