222aa3f4d44341a7f2dfbb7a67408f14ddf5c0c0
bashar-karoury <basharalkaroury@hotmail.com>
2024-01-30 23:34:44 +0400

    fix task 100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].scan(/\[from:(\w+)\].*/).join
+puts ARGV[0].scan(/\[from:\(.*)\].*\[to:.*\].*\[flags:.*\]\s\[msg/).join
 puts ","
 
-puts ARGV[0].scan(/\[from:\w+\].*\[to:(.*)\].*\[flags:.*\]\s\[msg/).join
+puts ARGV[0].scan(/\[from:\.*\].*\[to:(.*)\].*\[flags:.*\]\s\[msg/).join
 puts ","
 
-puts ARGV[0].scan(/\[from:\w+\].*\[to:.*\].*\[flags:(.*)\]\s\[msg/).join
+puts ARGV[0].scan(/\[from:\.*\].*\[to:.*\].*\[flags:(.*)\]\s\[msg/).join
 
