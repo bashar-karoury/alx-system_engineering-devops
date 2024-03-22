56be9332661f8be404e3e7b765b1accabbc82348
bashar-karoury <basharalkaroury@hotmail.com>
2024-03-22 17:31:53 +0400

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Add file with content to node
 file { '/tmp/school':
-  ensure     => file,
-  content    => 'I love Puppet',
-  owner      => 'www-data',
-  group      => 'www-data',
-  mode       => '0744'
+  ensure  => file,
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
 }
