77d3041075dd8892647660dcf225978485f1c998
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-05 09:48:52 +0300

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" script with a function to get info about TODOs of user id
+"""
+import sys
+import requests
+if sys.argv[1]:
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url)
+    # print("status code ", response.status_code)
+    todos = response.json()
+    completed = []
+    tasks_cout = 0
+    for todo in todos:
+        if todo.get('userId') == int(id):
+            tasks_cout += 1
+            if todo.get("completed") is True:
+                completed.append(todo.get("title"))
+
+    print("Employee {} is done with tasks({}/{}):".format(
+                                            id, len(completed), tasks_cout))
+    for item in completed:
+        print("\t", item)
