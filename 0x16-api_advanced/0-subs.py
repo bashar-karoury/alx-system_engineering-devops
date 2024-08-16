f4e6b378aca348a6fa207980991546d694ac1aba
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-16 22:09:32 +0300

    0x16-api_advanced fix task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,15 +3,13 @@
     the Reddit API and returns the number of subscribers
 """
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
     """ eturns the number of subscribers (not active users,
         total subscribers) for a given subreddit
     """
-    # print(sys.argv[1])
-    url = 'https://www.reddit.com/r/{}/about.json'.format(sys.argv[1])
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, allow_redirects=False)
 
     if response.status_code == 200:
