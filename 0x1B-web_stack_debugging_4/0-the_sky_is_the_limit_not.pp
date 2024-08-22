7a0c071efb94ec27a78e390e4229f505898a0c38
bashar-karoury <basharalkaroury@hotmail.com>
2024-08-23 00:12:28 +0300

    0x1B-web_stack_debugging_4 task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,11 @@
 # This Manifest fixs apache2 server 500 status code error
-$one = "sed -i 's/worker_processes 4;/worker_processes 40;/' /path/to/your/nginx.conf"
-$two = "$|require_once( ABSPATH . WPINC . '/class-wp-locale.php' );|\" /var/www/html/wp-settings.php"
-$value = "${one}${two}"
-exec { 'fix typo':
-  command => $one,
+$command = "sed -i 's/worker_processes 4;/worker_processes 40;/' /etc/nginx/nginx.conf"
+exec { 'raise limit of workers':
+  command => $command,
   path    => ['/bin', '/usr/bin'],
+}
+
+# Restart Nginx explicitly
+exec { 'restart_nginx':
+  command => '/usr/sbin/service nginx restart',
 }
